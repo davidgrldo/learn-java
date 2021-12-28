4f6febf227d06be5d6d1f0c65f4d047764620b5a
David Geraldo <50208233+davidgrldo@users.noreply.github.com>
2021-12-28 20:32:43 +0700

    second commit

---
+++ src/TipeDataString.java
@@ -0,0 +1,11 @@
+public class TipeDataString {
+  public static void main(String[] args) {
+    String firstName = "David";
+    String lastName = "Geraldo";
+    String fullName = firstName + " " + lastName;
+
+    System.out.println(firstName);
+    System.out.println(lastName);
+    System.out.println(fullName);
+  }
+}
