0c29da417fbdacda84537ef980af5b365ea2f4a1
David Geraldo <50208233+davidgrldo@users.noreply.github.com>
2022-01-01 09:37:11 +0700

    fix var error

---
+++ src/Block.java
@@ -0,0 +1,13 @@
+public class Block {
+  public static void main(String[] args) {
+    System.out.println("Hello world11");
+    System.out.println("Hello world12");
+    System.out.println("Hello world13");
+
+    {
+      System.out.println("Hello world14");
+      System.out.println("Hello world15");
+      System.out.println("Hello world16");
+    }
+  }
+}
